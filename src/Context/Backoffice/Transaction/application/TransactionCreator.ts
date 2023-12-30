288f80701058f5fdf012295eeadf15e83e84c560
Brando Vidal <yisusdracon@gmail.com>
2023-12-30 13:08:36 -0500

    🔨  use fromPrimitives method in Transaction

--- src/Context/Backoffice/Transaction/application/TransactionCreator.ts
+++ src/Context/Backoffice/Transaction/application/TransactionCreator.ts
@@ -10,7 +10,19 @@ export default class TransactionCreator {
 
   async run (seller_domain: string, kind: string, invoice_number: number, amount: number, total: number, status: string, user_created: string, user_updated: string) {
     const id = ObjectId.random()
-    const transaction = Transaction.create(id, seller_domain, kind, invoice_number, amount, total, status, user_created, user_updated)
+    const transaction = Transaction.fromPrimitives({
+      id,
+      seller_domain,
+      kind,
+      invoice_number,
+      amount,
+      total,
+      status,
+      user_created,
+      user_updated,
+      created_at: new Date(),
+      updated_at: new Date()
+    })
     await this.repository.save(transaction)
   }
 }
--- src/Context/Backoffice/Transaction/application/TransactionUpdator.ts
+++ src/Context/Backoffice/Transaction/application/TransactionUpdator.ts
@@ -7,7 +7,19 @@ export default class TransactionUpdator {
   constructor (@inject('TransactionRepository') private readonly repository: TransactionRepository) {}
 
   async run (id: string, seller_domain: string, kind: string, invoice_number: number, amount: number, total: number, status: string, user_created: string, user_updated: string) {
-    const transaction = Transaction.create(id, seller_domain, kind, invoice_number, amount, total, status, user_created, user_updated)
+    const transaction = Transaction.fromPrimitives({
+      id,
+      seller_domain,
+      kind,
+      invoice_number,
+      amount,
+      total,
+      status,
+      user_created,
+      user_updated,
+      created_at: undefined,
+      updated_at: new Date()
+    })
     await this.repository.update(transaction)
   }
 }
