058e0cbeda5f5175a2e7250f11e8bdf89d71f555
Brando Vidal <yisusdracon@gmail.com>
2023-12-30 15:40:47 -0500

    🔨  add more test

--- tests/transaction.test.ts
+++ tests/transaction.test.ts
@@ -5,8 +5,10 @@ import { describe, expect, it, beforeAll } from 'vitest'
 import { Transaction } from '../src/Context/Backoffice/Transaction/domain/Transaction'
 import type { TransactionRepository } from '../src/Context/Backoffice/Transaction/domain/TransactionRepository'
 
-import TransactionsFinder from '../src/Context/Backoffice/Transaction/application/TransactionsFinder'
-import TransactionCreator from '../src/Context/Backoffice/Transaction/application/TransactionCreator'
+import TransactionsFinder from 'src/Context/Backoffice/Transaction/application/TransactionsFinder'
+import TransactionCreator from 'src/Context/Backoffice/Transaction/application/TransactionCreator'
+import TransactionUpdator from 'src/Context/Backoffice/Transaction/application/TransactionUpdator'
+import TransactionDeletor from 'src/Context/Backoffice/Transaction/application/TransactionDeletor'
 
 class MockTransactionRepository implements TransactionRepository {
   private readonly transactions: Transaction[] = []
@@ -32,12 +34,16 @@ class MockTransactionRepository implements TransactionRepository {
 describe('Transaction', () => {
   let transactionsFinder: TransactionsFinder
   let transactionCreator: TransactionCreator
+  let transactionUpdator: TransactionUpdator
+  let transactionDeletor: TransactionDeletor
 
   beforeAll(() => {
     const repository = new MockTransactionRepository()
 
     transactionsFinder = new TransactionsFinder(repository)
     transactionCreator = new TransactionCreator(repository)
+    transactionUpdator = new TransactionUpdator(repository)
+    transactionDeletor = new TransactionDeletor(repository)
   })
 
   it('should get all transactions', async () => {
@@ -55,5 +61,23 @@ describe('Transaction', () => {
     const transactionExpected = transactionListExpected[0]
     expect(transactionExpected.sellerDomain).toBe(transaction.sellerDomain)
     expect(transactionExpected.amount).toBe(transaction.amount)
+  })
+
+  it('should update an transaction', async () => {
+    const transaction = new Transaction('1', 'example.com', 'WALLET', 1, 100, 100, 'PENDING', 'admin', 'admin', new Date(), new Date())
+    void transactionUpdator.run('1', 'example.com', 'WALLET', 1, 100, 100, 'PENDING', 'admin', 'admin')
+
+    const transactionListExpected = await transactionsFinder.run()
+    expect(transactionListExpected.length).toEqual(1)
+
+    const transactionExpected = transactionListExpected[0]
+    expect(transactionExpected.sellerDomain).toBe(transaction.sellerDomain)
+    expect(transactionExpected.amount).toBe(transaction.amount)
+  })
+
+  it('should delete an transaction', async () => {
+    void transactionDeletor.run('1')
+    const transactionListExpected = await transactionsFinder.run()
+    expect(transactionListExpected.length).toEqual(0)
   })
 })
