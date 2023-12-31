d77fe57240881b46b605199694f2ae8907b63eef
Brando Vidal <yisusdracon@gmail.com>
2023-12-31 10:29:55 -0500

    🧪  add FIXME

--- tests/status.test.ts
+++ tests/status.test.ts
@@ -10,6 +10,7 @@ let _request: request.Test
 let application: BackofficeBackendApp
 let _response: request.Response
 
+// FIXME: run test with Supertest
 describe('Backoffice tests', () => {
   it('I send a GET request to "status", the response status code should be 200', async () => {
     _request = await request(application.serverExpress).get('/v1/status')
