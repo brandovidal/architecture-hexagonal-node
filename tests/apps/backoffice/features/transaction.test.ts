c8427e3076cb524158268555f63ac14c5954a3fc
Brando Vidal <yisusdracon@gmail.com>
2024-01-06 07:48:05 -0500

    🧪  enable feature tests

--- tests/apps/backoffice/features/transaction.test.ts
+++ tests/apps/backoffice/features/transaction.test.ts
@@ -18,41 +18,40 @@ afterAll(async () => {
 describe('Check the transaction api', () => {
   it('I send a GET request to /v1/transactions, it should return 200', async () => {
     const response = await request(application.httpServer).get('/v1/transactions')
-    console.log('🚀 ~ file: transaction.test.ts:14 ~ it ~ response:', response.status, response.body)
 
     expect(response.statusCode).toEqual(200)
     expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
     expect(response.body.success).toEqual(true)
   })
 
-  // it('I send a POST request to /v1/transaction, it should return 201', async () => {
-  //   const response = await request(application.httpServer).post('/v1/transaction').send({
-  //     seller_domain: 'test.com',
-  //     kind: 'WALLET',
-  //     invoice_number: 8,
-  //     amount: 10,
-  //     status: 'PENDING',
-  //     user_created: 'test',
-  //     user_updated: 'test'
-  //   })
+  it('I send a POST request to /v1/transaction, it should return 201', async () => {
+    const response = await request(application.httpServer).post('/v1/transaction').send({
+      seller_domain: 'test.com',
+      kind: 'WALLET',
+      invoice_number: 8,
+      amount: 10,
+      status: 'PENDING',
+      user_created: 'test',
+      user_updated: 'test'
+    })
 
-  //   expect(response.statusCode).toEqual(201)
-  //   expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
-  //   expect(response.body.success).toEqual(true)
-  // })
+    expect(response.statusCode).toEqual(201)
+    expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
+    expect(response.body.success).toEqual(true)
+  })
 
-  // it('I send a DELETE request to /v1/transaction, it should return 200', async () => {
-  //   const response = await request(application.httpServer).get('/v1/transactions')
+  it('I send a DELETE request to /v1/transaction, it should return 200', async () => {
+    const response = await request(application.httpServer).get('/v1/transactions')
 
-  //   const transactions = response.body.data
+    const transactions = response.body.data
 
-  //   expect(response.statusCode).toEqual(200)
-  //   expect(transactions.length).toBeGreaterThan(0)
+    expect(response.statusCode).toEqual(200)
+    expect(transactions.length).toBeGreaterThan(0)
 
-  //   const transactionId = transactions[0].id
+    const transactionId = transactions[0].id
 
-  //   await request(application.httpServer).delete(`/v1/transaction/${transactionId}`).send({
-  //     id: transactionId
-  //   })
-  // })
+    await request(application.httpServer).delete(`/v1/transaction/${transactionId}`).send({
+      id: transactionId
+    })
+  })
 })
