ae2b44b375964f754d0ecd643b559812df57fcd3
brando vidal <yisusdracon@gmail.com>
2024-01-05 21:26:48 -0500

    🔨  implement validation schema with zod

--- src/apps/backoffice/routes/transaction.route.ts
+++ src/apps/backoffice/routes/transaction.route.ts
@@ -7,12 +7,21 @@ import TransactionPostController from '../controllers/TransactionPostController'
 import TransactionPutController from '../controllers/TransactionPutController'
 import TransactionDeleteController from '../controllers/TransactionDeleteController'
 
+import { object, string } from 'zod'
+import validationRequestSchema from '../middlewares/validationRequestSchema'
+
+const validationCreateSchema = object({
+  body: object({
+    seller_domain: string()
+  })
+})
+
 function register (router: Router) {
   const transactionGetController = container.resolve(TransactionsGetController)
   router.get('/transactions', (req: Request, res: Response) => transactionGetController.run(req, res))
 
   const transactionPostController = container.resolve(TransactionPostController)
-  router.post('/transaction', (req: Request, res: Response) => transactionPostController.run(req, res))
+  router.post('/transaction', validationRequestSchema(validationCreateSchema), (req: Request, res: Response) => transactionPostController.run(req, res))
 
   const transactionPutController = container.resolve(TransactionPutController)
   router.put('/transaction/:id', (req: Request, res: Response) => transactionPutController.run(req, res))
